5d8cf9e14039eca5a47497cc90f59cf893febbae
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2026-01-06 21:00:57 +0500

    Create Root route for Header component to be in every page

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,26 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./main.css";
 import Home, { puzzlesLoader } from "./routes/Home/Home";
+import Puzzle, { puzzleByIdLoader } from "./routes/Puzzle/Puzzle";
+import Root from "./routes/Root";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Home />,
-    loader: puzzlesLoader,
+    element: <Root />,
+
+    children: [
+      {
+        index: true,
+        element: <Home />,
+        loader: puzzlesLoader,
+      },
+      {
+        path: "/puzzles/:puzzleId",
+        element: <Puzzle />,
+        loader: puzzleByIdLoader,
+      },
+    ],
   },
 ]);
 
