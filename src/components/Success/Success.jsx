a9d5d12fd4da917fdd946b7cc49c4cf84bfd503f
WillBarrent <161952799+WillBarrent@users.noreply.github.com>
2026-01-19 21:29:55 +0500

    Implement onSubmit function when user fills username field and submit

--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -1,13 +1,36 @@
+import { useState } from "react";
 import styles from "./Success.module.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
-function Success({ completionTime }) {
+function Success({ completionTime, puzzleId, playerId }) {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+
+  async function onSubmit() {
+    "use server";
+
+    const data = await fetch("http://localhost:3000/leaderboard", {
+      method: "PUT",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username,
+        playerId: playerId,
+        puzzleId: puzzleId,
+      }),
+    });
+
+    return navigate("/");
+  }
+
   return (
     <div className={styles.completed}>
       <div className={styles.completedTitle}>
         You've completed puzzle in {completionTime} seconds. Congratulations!
       </div>
-      <form action="" className={styles.submitForm}>
+      <form action={onSubmit} className={styles.submitForm}>
         <div className={styles.yourName}>
           <label htmlFor="name">Your name</label>
           <input
@@ -15,9 +38,15 @@ function Success({ completionTime }) {
             type="text"
             className={styles.nameInput}
             placeholder="Waldo"
+            value={username}
+            onChange={(e) => {
+              setUsername(e.target.value);
+            }}
           />
         </div>
-        <button className={styles.submitYourName}>Submit</button>
+        <button type="submit" className={styles.submitYourName}>
+          Submit
+        </button>
       </form>
       <Link className={styles.backToHomeLink} to={"/"}>
         Back to Home Page
